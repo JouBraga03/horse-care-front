7e9afd8ceeda00567f3f6888db62ba35537c16db
Jou <jonathan03.braga@gmail.com>
2024-09-03 19:00:24 -0300

    chore: create logo and home

--- src/config/layout/base/components/body/body.tsx
+++ src/config/layout/base/components/body/body.tsx
@@ -4,8 +4,6 @@ const Body = ({ children }: BodyProps) => {
   return (
     <main className="flex-1 flex items-start justify-start p-4 w-full h-full">
       <div className="bg-white w-full h-full">
-        <h2 className="text-xl font-semibold mb-4">Route Name</h2>
-
         {children}
       </div>
     </main>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,17 @@
+import clsx from 'clsx'
+import HorseCare from '../../assets/horse_care.png'
+
+import style from './style.module.css'
+
 const Home = () => {
   return (
     <>
-      <div>
-        <h1> olar </h1>
+      <div className='w-full h-full flex items-center justify-center flex-col'>
+        <picture>
+          <img src={HorseCare} alt="horse care logo" />
+        </picture>
+
+        <h2 className={clsx('font-bold text-5xl', style.title)}> Bem vindo a Vet Care </h2>
       </div>
     </>
   )
